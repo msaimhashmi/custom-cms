c20c7adc42b2b19cc25c4b1b9f5c22dd3310c34b
M Saim Hashmii <saimhashmii433@gmail.com>
2021-08-16 15:41:21 +0500

    first commit

---
+++ resources/views/admin/layout/header.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <title>Flatkit - HTML Version | @yield('page-title')</title>
+  <link rel="canonical" href="{{url()->current()}}"/>
+  <meta name="description" content="@yield('meta-description')"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimal-ui" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+  <!-- for ios 7 style, multi-resolution icon of 152x152 -->
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-barstyle" content="black-translucent">
+  <link rel="apple-touch-icon" href="/admin/assets/images/logo.png">
+  <meta name="apple-mobile-web-app-title" content="Flatkit">
+  <!-- for Chrome on Android, multi-resolution icon of 196x196 -->
+  <meta name="mobile-web-app-capable" content="yes">
+  <link rel="shortcut icon" sizes="196x196" href="/admin/assets/images/logo.png">
+  
+  <!-- style -->
+  <link rel="stylesheet" href="/admin/assets/animate.css/animate.min.css" type="text/css" />
+  <link rel="stylesheet" href="/admin/assets/glyphicons/glyphicons.css" type="text/css" />
+  <link rel="stylesheet" href="/admin/assets/font-awesome/css/font-awesome.min.css" type="text/css" />
+  <link rel="stylesheet" href="/admin/assets/material-design-icons/material-design-icons.css" type="text/css" />
+  <link rel="stylesheet" href="/admin/assets/bootstrap/dist/css/bootstrap.min.css" type="text/css" />
+  <!-- build:css /admin/assets/styles/app.min.css -->
+  <link rel="stylesheet" href="/admin/assets/styles/app.css" type="text/css" />
+  <!-- endbuild -->
+  <link rel="stylesheet" href="/admin/assets/styles/font.css" type="text/css" />
+</head>
---
+++ resources/views/admin/layout/master.blade.php
@@ -0,0 +1,60 @@
+@include('admin.layout.header')
+<body>
+  <div class="app" id="app">
+    <!-- ############ LAYOUT START-->
+    <!-- aside -->
+    @include('admin.layout.sidebar')
+    <!-- / -->
+    
+    <!-- content -->
+    <div id="content" class="app-content box-shadow-z0" role="main">
+
+      {{-- NAVBAR START --}}
+      @include('admin.layout.navbar')
+      {{-- NAVBAR END --}}
+
+      {{-- FOOTER START --}}
+      @include('admin.layout.footer')
+      {{-- FOOTER END --}}
+
+      <div ui-view class="app-body" id="view">
+        <!-- ############ PAGE START-->
+        @section('main-content')
+          @show
+        <!-- ############ PAGE END-->
+      </div>
+    </div>
+    <!-- / -->
+    <!-- theme switcher -->
+    @include('admin.layout.switcher')
+    <!-- / -->
+    <!-- ############ LAYOUT END-->
+  </div>
+  <!-- build:js /admin/scripts/app.html.js -->
+  <!-- jQuery -->
+  <script src="/admin/libs/jquery/jquery/dist/jquery.js"></script>
+  <!-- Bootstrap -->
+  <script src="/admin/libs/jquery/tether/dist/js/tether.min.js"></script>
+  <script src="/admin/libs/jquery/bootstrap/dist/js/bootstrap.js"></script>
+  <!-- core -->
+  <script src="/admin/libs/jquery/underscore/underscore-min.js"></script>
+  <script src="/admin/libs/jquery/jQuery-Storage-API/jquery.storageapi.min.js"></script>
+  <script src="/admin/libs/jquery/PACE/pace.min.js"></script>
+  <script src="/admin/scripts/config.lazyload.js"></script>
+  <script src="/admin/scripts/palette.js"></script>
+  <script src="/admin/scripts/ui-load.js"></script>
+  <script src="/admin/scripts/ui-jp.js"></script>
+  <script src="/admin/scripts/ui-include.js"></script>
+  <script src="/admin/scripts/ui-device.js"></script>
+  <script src="/admin/scripts/ui-form.js"></script>
+  <script src="/admin/scripts/ui-nav.js"></script>
+  <script src="/admin/scripts/ui-screenfull.js"></script>
+  <script src="/admin/scripts/ui-scroll-to.js"></script>
+  <script src="/admin/scripts/ui-toggle-class.js"></script>
+  <script src="/admin/scripts/app.js"></script>
+  <!-- ajax -->
+  <script src="/admin/libs/jquery/jquery-pjax/jquery.pjax.js"></script>
+  <script src="/admin/scripts/ajax.js"></script>
+  <!-- endbuild -->
+</body>
+</html>
